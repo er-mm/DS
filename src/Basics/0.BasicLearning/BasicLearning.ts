3a98b9c9fcfffe4d96d0a423a6322a526247aea6
Mayank Mittal <mayank.mittal@allocatesoftware.com>
2020-05-14 02:56:21 +0530

    changing paths

---
+++ src/Basics/0.BasicLearning/BasicLearning.ts
@@ -0,0 +1,20 @@
+const basics = `
+	Will learn the basics First Before moving to DataStructures. 
+	Their are 15 Topics which will cover the basics.
+	1. HelloWorld
+	2. StringReversal
+	3. Palindromes
+	4. IntegerReversal
+	5. MaxChars
+	6. TheClassicFizzBuzz
+	7. ArrayChunking
+	8. Anagrams
+	9. SentenceCapitalization
+	10. PriintingSteps
+	11. Pyramids
+	12. FindVowels
+	13. MatrixSpiral
+	14. RunTimeComplexity
+	15. FibnocciSeries
+`;
+console.log(basics);
